74d8a72171a0ec6c6da8e1b46851131b003332ac
fire855 <thefire855@gmail.com>
2016-03-27 21:19:45 +0200

    RIL fixes

--- device.mk
+++ device.mk
@@ -188,7 +188,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.kernel.android.checkjni=0 \
-    ro.telephony.ril_class=MediaTekRIL \
     ro.telephony.ril.config=fakeiccid  \
     persist.call_recording.enabled=true \
     persist.call_recording.src=1 \
