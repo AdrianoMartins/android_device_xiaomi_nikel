eb0c57db286afaa199453ddfa75146d3d87793d9
fire855 <thefire855@gmail.com>
2016-09-05 18:10:05 +0200

    Remove prebuilt kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,6 @@ BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --base 0x40078000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x03f88000 --second_offset 0x00e88000 --tags_offset 0x0df88000 --board 1450352440
-#TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
 TARGET_KERNEL_SOURCE := kernel/jiayu/s3plus_n560a
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
@@ -121,7 +120,6 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storag
 
 # TWRP stuff
 ifeq ($(RECOVERY_VARIANT), twrp)
-TARGET_PREBUILT_RECOVERY_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 DEVICE_RESOLUTION := 1080x1920
 DEVICE_SCREEN_WIDTH := 1080
