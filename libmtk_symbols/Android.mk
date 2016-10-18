08e49465b877a34a177ffc08cfc5d61dbb6a28c3
fire855 <thefire855@gmail.com>
2016-10-18 21:24:26 +0200

    mtk_symbols: Remove unused symbols

--- libmtk_symbols/Android.mk
+++ libmtk_symbols/Android.mk
@@ -5,15 +5,12 @@ LOCAL_SRC_FILES := \
     mtk_xlog.cpp \
     mtk_gui.cpp \
     mtk_ui.cpp \
-    mtk_omx.cpp \
     mtk_audio.cpp
 
-# only for 32bit libraries
-LOCAL_SRC_FILES_32 := mtk_string.cpp
 # only for 64bit libraries
 LOCAL_SRC_FILES_64 := mtk_parcel.cpp
 
-LOCAL_SHARED_LIBRARIES := libbinder libutils liblog libgui libui
+LOCAL_SHARED_LIBRARIES := libbinder liblog libgui libui
 LOCAL_MODULE := libmtk_symbols
 LOCAL_MODULE_TAGS := optional
 
